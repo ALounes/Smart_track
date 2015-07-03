f3da8a741176eedd0e46cb5b3762446a2fce44c3
Achab Lounes <achab.lounes@gmail.com>
2015-07-03 16:52:50 +0200

    beta 3.312 ,sup de welcome

--- app/src/main/java/diten/smart_track/MainActivity.java
+++ app/src/main/java/diten/smart_track/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.welcome);
+        setContentView(R.layout.activity_main);
 
     }
 }
